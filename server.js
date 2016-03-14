0fad91cc3bfe736900a2dae45d21510f30debf44
Suseendranbabu <susee.psg@gmail.com>
2016-03-14 23:28:26 +0530

    14th March

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 // Create an express object
 var express = require('express'),
 	stylus = require('stylus'),
-	logger = require('morgan');
+	logger = require('morgan'),
+	bodyparser = require('body-parser');
 
 // Set the node environmental variable to development
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+// This code is to use the stylus
+{
+	// Create a middleware function
+
+	var compile = function(str, path)
+	{
+		return stylus(str).set('filename', path);
+	}
+}
 // Create the express object
 var app = express();
 
@@ -14,6 +24,19 @@ app.set('views' , __dirname + '/server/view');
 
 // Set the view engine
 app.set('view engine', 'jade');
+app.use(stylus.middleware(
+{
+	src : __dirname + '/public',
+	compile : compile
+
+}));
+
+// If any unknown route comes, the route will be searched in the public folder
+// and if a file matches the route it will be returned to the user.
+app.use(express.static(__dirname + '/public'));
+app.use(logger('dev'));
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyparser.json());
 
 // This code can be deleted 
 	{
